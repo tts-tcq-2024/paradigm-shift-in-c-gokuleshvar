6993a07601bff72e7dcdc27132a05365c4448155
GOK5COB <kirubhakar.gokuleshvar@in.bosch.com>
2024-06-24 17:04:39 +0530

    - Update for CC improvement v5

--- .github/workflows/checker.c
+++ .github/workflows/checker.c
@@ -7,6 +7,7 @@
 #define LOW_STR     "Low"
 #define HIGH_STR    "High"
 
+
 // Function to display error messages
 void dispError(char *param, char *breach) {
 	printf("%s out of range: %s\n", param, breach);
@@ -15,10 +16,10 @@ void dispError(char *param, char *breach) {
 // Function to check temperature range
 int checkTempRange(float temperature) {
 	int tempValid_F = 1;
-	if(temperature < 0) {
+	if(temperature < 0.0f) {
 		dispError(TEMP_STR, LOW_STR);
 		tempValid_F = 0;
-	} else if(temperature > 45) {
+	} else if(temperature > 45.0f) {
 		dispError(TEMP_STR, HIGH_STR);
 		tempValid_F = 0;
 	}
@@ -31,10 +32,10 @@ int checkTempRange(float temperature) {
 // Function to check state of charge range
 int checkSocRange(float soc) {
 	int socValid_F = 1;
-    if(soc < 20) {
+    if(soc < 20.0f) {
 		dispError(SOC_STR, LOW_STR);
 		socValid_F = 0;
-	} else if(soc > 80) {
+	} else if(soc > 80.0f) {
 		dispError(SOC_STR, HIGH_STR);
 		socValid_F = 0;
 	}
@@ -47,7 +48,7 @@ int checkSocRange(float soc) {
 // Function to check charge rate range
 int checkCrRange(float chargeRate) {
 	int crValid_F = 1;
-	if(chargeRate > 0.8) {
+	if(chargeRate > 0.8f) {
 	    dispError(CR_STR, HIGH_STR);
 		crValid_F = 0;
 	}
@@ -65,15 +66,15 @@ int batteryIsOk(float temperature, float soc, float chargeRate) {
 // Tests
 int main() {
   assert(!batteryIsOk(-5.0, 70, 0.7));  // Low temperature
-  assert(!batteryIsOk(0, 70, 0.7));     // Boundary temperature - Low
+  assert(batteryIsOk(0, 70, 0.7));     	// Boundary temperature - Lower
   assert(!batteryIsOk(50, 70, 0.7));    // High temperature
-  assert(!batteryIsOk(45, 70, 0.7));    // Boundary temperature - High
+  assert(batteryIsOk(45, 70, 0.7));    	// Boundary temperature - Upper
   assert(!batteryIsOk(25, 10, 0.7));    // Low SOC
-  assert(!batteryIsOk(25, 20, 0.7));    // Boundary SOC - Low
+  assert(batteryIsOk(25, 20, 0.7));    	// Boundary SOC - Lower
   assert(!batteryIsOk(25, 90, 0.7));    // High SOC
-  assert(!batteryIsOk(25, 80, 0.7));    // Boundary SOC - High
+  assert(batteryIsOk(25, 80, 0.7));    	// Boundary SOC - Upper
   assert(!batteryIsOk(25, 70, 0.9));    // High charge rate
-  assert(!batteryIsOk(25, 70, 0.8));    // Boundary charge rate
+  assert(batteryIsOk(25, 70, 0.8));    	// Boundary charge rate
   assert(!batteryIsOk(50, 90, 0.9));    // All parameters out of range
   assert(batteryIsOk(25, 70, 0.7));     // Normal parameters
 }
