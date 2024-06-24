1080fddb13207eea853381bc824b43501a391a8f
GOK5COB <kirubhakar.gokuleshvar@in.bosch.com>
2024-06-24 12:21:16 +0530

    - Update for CC improvement v1

--- checker.c
+++ checker.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
 
+int checkTempRange(float temperature)
+{
+	return (temperature < 0 || temperature > 45);
+}
+
+int checkSocRange(float soc)
+{
+	return (soc < 20 || soc > 80);
+}
+
+int checkCrRange(float chargeRate)
+{
+	return (chargeRate > 0.8);
+}
+
+
 int batteryIsOk(float temperature, float soc, float chargeRate) {
-  if(temperature < 0 || temperature > 45) {
+  if(checkTempRange(temperature)) {
     printf("Temperature out of range!\n");
     return 0;
-  } else if(soc < 20 || soc > 80) {
+  }
+
+  if(checkSocRange(soc)) {
     printf("State of Charge out of range!\n");
     return 0;
-  } else if(chargeRate > 0.8) {
+  }
+
+  if(checkCrRange(chargeRate)) {
     printf("Charge Rate out of range!\n");
     return 0;
   }
+
   return 1;
 }
 
